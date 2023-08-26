5a5f47675b8c2086da86122afc6882e85165448f
Jamesie1234 <james.adesanlu@news.co.uk>
2023-08-26 11:04:11 +0100

    added more comments

--- variables.tf
+++ variables.tf
@@ -1,7 +1,9 @@
 variable "org_name" {
+  decription = "Enter your organization name "
   #default = "voxsmart-play" #replace with your organization name
 }
 
 variable "github_personal_token" {
+  decription = "Enter your github personal access token "
   #default = "Your-github-personal-access-token" #Your github personal access token
 }
